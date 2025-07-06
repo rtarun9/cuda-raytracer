1058348ca4f90f0c5db2a95ba5c10265122c21aa
rtarun9 <rtarun0913@gmail.com>
2025-07-06 15:17:17 +0530

    Added basic sdl window with code to render to texture

--- src/main.cu
+++ src/main.cu
@@ -1,44 +1,66 @@
+#include <algorithm>
 #include <cuda.h>
 #include <iostream>
 
-__global__ void add_vectors(float *vec_a, float *vec_b, size_t N, float *output)
-{
-    uint32_t thread_id =threadIdx.x + blockIdx.x * blockDim.x;
-    if (thread_id < N)
-    {
-        output[thread_id] = vec_a[thread_id] + vec_b[thread_id];
-    }
-}
+#define SDL_MAIN_HANDLED
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-int main()
+int main(int argc, char **argv)
 {
     cudaDeviceProp device_prop = {};
     cudaGetDeviceProperties(&device_prop, 0);
     std::cout << "Cuda device name :: " << device_prop.name << '\n';
-    constexpr size_t N  = 5;
 
-    float vec_a[] = {1, 2, 3, 4, 5};
-    float vec_b[] = {-1.0f, -2.0f, -5.0f, 0.0f, 10.0f};
-    float result[] = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
+    // Setup SDL.
+    constexpr float ASPECT_RATIO = 16.0f / 9.0f;
+    constexpr uint32_t WINDOW_WIDTH = 1080;
+    constexpr uint32_t WINDOW_HEIGHT = static_cast<uint32_t>((float)WINDOW_WIDTH / ASPECT_RATIO);
 
-    float* dev_vec_a = nullptr;
-    float* dev_vec_b = nullptr;
-    float* dev_result = nullptr;
-    cudaMalloc((void**)&dev_vec_a, sizeof(float) * N);
-    cudaMalloc((void**)&dev_vec_b, sizeof(float) * N);
-    cudaMalloc((void**)&dev_result, sizeof(float) * N);
-
-    cudaMemcpy(dev_vec_a, vec_a, sizeof(float) * N, cudaMemcpyKind::cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_vec_b, vec_b, sizeof(float) * N, cudaMemcpyKind::cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_result, result, sizeof(float) * N, cudaMemcpyKind::cudaMemcpyHostToDevice);
-
-    add_vectors<<<1, N>>>(dev_vec_a, dev_vec_b, N, dev_result);
-    cudaDeviceSynchronize();
-
-    cudaMemcpy(result, dev_result, sizeof(float) * N, cudaMemcpyKind::cudaMemcpyDeviceToHost);
-    for (size_t i = 0; i < N; i++)
+    // SDL initialization and window creation.
+    if (!SDL_Init(SDL_INIT_VIDEO))
     {
-        std::cout << "result[" <<  i << "] = " << result[i] << '\n';
+        SDL_Log("SDL_Init failed (%s)", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    SDL_Window *sdl_window =
+        SDL_CreateWindow("cuda-raytracer", static_cast<int>(WINDOW_WIDTH), static_cast<int>(WINDOW_HEIGHT), 0);
+    if (sdl_window == nullptr)
+    {
+        SDL_Log("SDL_CreateWindow failed (%s)", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    SDL_Renderer *sdl_renderer = SDL_CreateRenderer(sdl_window, nullptr);
+    SDL_Texture *sdl_texture = SDL_CreateTexture(sdl_renderer, SDL_PixelFormat::SDL_PIXELFORMAT_RGB24,
+                                                 SDL_TEXTUREACCESS_STREAMING, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    bool quit = false;
+    while (!quit)
+    {
+        SDL_Event event = {};
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_EVENT_QUIT)
+            {
+                quit = true;
+            }
+        }
+
+        uint8_t *pixels = nullptr;
+        int pitch = sizeof(float) * WINDOW_WIDTH;
+
+        SDL_LockTexture(sdl_texture, nullptr, (void **)&pixels, &pitch);
+        for (size_t i = 0; i < WINDOW_WIDTH * WINDOW_HEIGHT; i++)
+        {
+            *pixels++ = 0xff;
+        }
+        SDL_UnlockTexture(sdl_texture);
+
+        SDL_RenderClear(sdl_renderer);
+        SDL_RenderTexture(sdl_renderer, sdl_texture, nullptr, nullptr);
+        SDL_RenderPresent(sdl_renderer);
     }
 
     return 0;
